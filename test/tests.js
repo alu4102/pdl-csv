5e10abcea7e6ae4dfde4ff280a0b8fdea4a60e36
alu4102 <alu0100537123@ull.edu.es>
2014-02-13 13:01:07 +0000

    Añadidas las pruebas

--- test/tests.js
+++ test/tests.js
@@ -1,19 +1,29 @@
 var assert = chai.assert;
 
-suite('temperature', function() {
-    test('32F = 0C', function() {
-        original.value = "32F";
+suite('csv', function() {
+    test('Las tablas se crean correctamente_1', function () {
+        original.value = 'a,"b, c",d,e\nf,g,h,i';
         calculate();
-        assert.deepEqual(converted.innerHTML, "0.0 Celsius");
+        var expected = '\n<table id="result" class="center">\n\n<tbody><tr class="">\n\n<td>a</td>\n\n<td>b, c</td>\n            \n              <td>d</td>\n            \n              <td>e</td>\n            \n          </tr>\n        \n          <tr class="">\n            \n              <td>f</td>\n            \n              <td>g</td>\n            \n              <td>h</td>\n            \n              <td>i</td>\n            \n          </tr>\n        \n      </tbody></table>\n    ';
+        assert.deepEqual(finaltable.innerHTML, expected);
     });
-    test('45C = 113.0 Farenheit', function() {
-        original.value = "45C";
+    test('Las tablas se crean correctamente_2', function () {
+        original.value = "a,b c,c,d\ne\\\"_,f,g,h\ni,j,k,l,l";
         calculate();
-        assert.deepEqual(converted.innerHTML, "113.0 Farenheit");
+        var expected = '\n      <table id="result" class="center">\n        \n          <tbody><tr class="">\n            \n              <td>a</td>\n            \n              <td>b c</td>\n            \n              <td>c</td>\n            \n              <td>d</td>\n            \n          </tr>\n        \n          <tr class="">\n            \n              <td>e\"_</td>\n            \n              <td>f</td>\n            \n              <td>g</td>\n            \n              <td>h</td>\n            \n          </tr>\n        \n          <tr class="error">\n            \n              <td>i</td>\n            \n              <td>j</td>\n            \n              <td>k</td>\n            \n              <td>l</td>\n            \n              <td>l</td>\n            \n          </tr>\n        \n      </tbody></table>\n    ';
+        assert.deepEqual(finaltable.innerHTML, expected);
     });
-    test('5X = error', function() {
-        original.value = "5X";
+    test('Funciona el almacenamiento local', function () {
+        assert.deepEqual(localStorage.original, "a,b c,c,d\ne\\\"_,f,g,h\ni,j,k,l,l");
+    });
+	test('Entrada erronea', function() {
+        original.value = '"producto", "precio" \n "camisa", "4,3", 44';
         calculate();
-        assert.match(converted.innerHTML, /ERROR/);
+        assert.match(finaltable.innerHTML, /class="error"/);
+    });
+	test('Entrada erronea', function() {
+        original.value = '"producto", "precio" \n "camisa", "4,3", 44';
+        calculate();
+        assert.match(finaltable.innerHTML, /class="error"/);
     });
 });
